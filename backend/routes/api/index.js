e119423f1ca67341b3fd30bf74f828ab3f057e39
railind <LindquistSocial@gmail.com>
2023-11-26 12:57:59 -0600

    New stuff

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -41,6 +42,15 @@ router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
 
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 
 module.exports = router;
