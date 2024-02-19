b1c5f37db0b2e4a03f72e2cf19578e9f98f09f7f
railind <LindquistSocial@gmail.com>
2024-02-18 18:55:54 -0600

    newest version

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const groupsRouter = require('./groups.js')
@@ -7,40 +6,6 @@ const venuesRouter = require('./venues.js')
 const eventsRouter = require('./events.js')
 const groupImageRouter = require('./group-images.js')
 const eventImageRouter = require('./event-images.js')
-
-
-
-if (process.env.NODE_ENV === 'production') {
-    const path = require('path');
-    // Serve the frontend's index.html file at the root route
-    router.get('/', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
-            path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
-        );
-    });
-
-    // Serve the static assets in the frontend's build folder
-    router.use(express.static(path.resolve("../frontend/build")));
-
-    // Serve the frontend's index.html file at all other routes NOT starting with /api
-    router.get(/^(?!\/?api).*/, (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
-            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-        );
-    });
-}
-
-
-if (process.env.NODE_ENV !== 'production') {
-    router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
-    });
-}
-
-
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
 
