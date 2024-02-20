10cc42f505b49023379c3905b0647fb4dadcc01a
railind <LindquistSocial@gmail.com>
2024-02-20 00:59:29 -0600

    Trouble with groups

--- frontend/src/store/groups.js
+++ frontend/src/store/groups.js
@@ -1,1 +1,18 @@
 import { csrfFetch } from './csrf';
+
+
+export const LOAD_GROUPS = 'groups/LOAD_GROUPS'
+
+
+export const loadGroups = (groups) => ({
+    type: LOAD_GROUPS,
+    groups
+})
+
+
+
+export const thunkingGroup = () => async (dispatch) => {
+    const response = await fetch('/api/groups')
+    const groups = await response.json()
+    dispatch(loadGroups(groups))
+}
