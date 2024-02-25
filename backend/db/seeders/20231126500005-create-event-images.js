e09c84a2e6f10b935dfc28385a8b885ac0c48395
railind <LindquistSocial@gmail.com>
2024-02-25 01:55:50 -0600

    new images

--- backend/db/seeders/20231126500005-create-event-images.js
+++ backend/db/seeders/20231126500005-create-event-images.js
@@ -22,14 +22,21 @@ module.exports = {
     await EventImage.bulkCreate([
       {
         eventId: 1,
-        url: 'www.coolpicture/27',
+        url: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhDMkowWU_BWUo7MDntEpURxK6_5MLgD9x5as1uLHo7f2Q7fxHerMHvtq9EIbWMpGpgLo9nUML1YUMqpPTBjA9P4rAYkNXQVCTVCRmaFJ4zQKu6dnMdtZL0dM7ZqasqD1_UgfYNfuqHuUBtdo7scnS73olBccARjLCpVlI3QbELM-p10Z8EhsPaqk221A/s1600/APEX-LEGENDS-WALLPAPER.jpg',
         preview: true
       },
       {
         eventId: 1,
         url: 'www.coolpicture/212',
+        preview: false
+      },
+      {
+        eventId: 2,
+        url: 'https://images.wallpapersden.com/image/download/apex-legends-hd-cool-gaming-2022-art_bWlmbWaUmZqaraWkpJRnZWltrWZmamc.jpg',
         preview: true
       }
+
+
     ], { validate: true });
   },
 
